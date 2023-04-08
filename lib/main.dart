d692988dccff0a932f20b7533e4a0a2019320337
Mohammad Abd-Elmoniem <98095976+madamoniem@users.noreply.github.com>
2023-04-08 11:21:04 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,30 @@
-// Import necessary packages
-import 'package:flutter/material.dart'; // Flutter Material Design UI components
-import 'package:flutter_bitcamp/explore_destinations.dart'; // ExploreDestinations class
-import 'package:google_fonts/google_fonts.dart'; // Google Fonts package
+// We need to use these special tools to make our app.
+import 'package:flutter/material.dart';
+import 'package:flutter_bitcamp/explore_destinations.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+// This is where our app starts.
 void main() {
-  runApp(const MyApp()); // Start the app by running the MyApp widget
+  runApp(const MyApp());
 }
 
-// MyApp widget
+// This is the main part of our app.
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key); // Constructor
+  // This is a magic word that helps our app work well.
+  const MyApp({Key? key}) : super(key: key);
 
+  // This tells our app how to draw and look nice.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // This helps our app use some new, cool things.
       theme: ThemeData(
-        useMaterial3: true, // Use Material3 theme
+        useMaterial3: true,
       ),
-      home: const ExploreDestinations(), // Set ExploreDestinations as the home screen
-      debugShowCheckedModeBanner: false, // Hide debug banner
+      // This is the first thing we see when we open the app.
+      home: const ExploreDestinations(),
+      // This makes sure we don't see a little banner in the corner.
+      debugShowCheckedModeBanner: false,
     );
   }
 }

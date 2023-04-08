3dbe61ee3df47b87c882d6c085ad9ebb0929e07f
Mohammad Abd-Elmoniem <98095976+madamoniem@users.noreply.github.com>
2023-04-08 11:00:46 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bitcamp/explore_destinations.dart';
-import 'package:google_fonts/google_fonts.dart';
+// Import necessary packages
+import 'package:flutter/material.dart'; // Flutter Material Design UI components
+import 'package:flutter_bitcamp/explore_destinations.dart'; // ExploreDestinations class
+import 'package:google_fonts/google_fonts.dart'; // Google Fonts package
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // Start the app by running the MyApp widget
 }
 
-// open ExploreDestinations class
-
+// MyApp widget
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key); // Constructor
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        useMaterial3: true,
+        useMaterial3: true, // Use Material3 theme
       ),
-      home: const ExploreDestinations(),
-      debugShowCheckedModeBanner: false,
+      home: const ExploreDestinations(), // Set ExploreDestinations as the home screen
+      debugShowCheckedModeBanner: false, // Hide debug banner
     );
   }
 }
